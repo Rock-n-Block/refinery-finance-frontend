635474d774fd8dba08edb39dc39f7586383c4706
TheJuze <dimanikus0106@gmail.com>
2021-03-28 18:57:45 +0300

    init test env

--- jest.config.js
+++ jest.config.js
@@ -1,3 +1,4 @@
+/*
 module.exports = {
   "roots": ['<rootDir>/src'],
   "transform": {
@@ -11,4 +12,42 @@ module.exports = {
     "^.+\\.svg": "<rootDir>/src/__mocks__/svgrMock.tsx",
     "\\.(css|less|sass|scss)$": "<rootDir>/src/__mocks__/styleMock.ts",
   },
+};
+*/
+
+const path = require('path');
+
+module.exports = {
+    verbose: true,
+    setupFilesAfterEnv: ['<rootDir>/src/config/setupTests.ts'],
+    moduleNameMapper: {
+        '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$': path.join(
+            __dirname,
+            './mocks/file-mock.js',
+        ),
+        '\\.(css|less)$': path.join(__dirname, './mocks/style-mock.js'),
+        '^@/(.*)$': '<rootDir>/src/$1',
+    },
+    testMatch: ['**/*.test.{ts,tsx}'],
+    modulePathIgnorePatterns: ['<rootDir>/dist/'],
+    coveragePathIgnorePatterns: ['/node_modules/', '/coverage/', '/types/'],
+    collectCoverageFrom: ['**/*.{ts,tsx}'],
+    reporters: [
+        'default',
+        [
+            './node_modules/jest-html-reporter',
+            {
+                pageTitle: 'Test Report ',
+                outputPath: './reports/test-report.html',
+                includeFailureMsg: true,
+            },
+        ],
+        [
+            'jest-junit',
+            {
+                outputDirectory: './reports',
+                suiteName: 'Jest Tests',
+            },
+        ],
+    ],
 };
