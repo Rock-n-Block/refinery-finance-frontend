bebbcbbf4abd33e69e40507951dda435056c3b4f
kalinichenko <ka.ki.ko.1995@gmail.com>
2022-01-17 10:36:34 +0300

    added radio changer of content page

--- src/components/sections/Profile/Content/index.tsx
+++ src/components/sections/Profile/Content/index.tsx
@@ -1,14 +1,14 @@
-import { FC, useState } from 'react';
+import { FC, useCallback, useEffect, useState } from 'react';
+import { RadioChangeEvent } from 'antd';
 
 import TokenIcon from '@/assets/img/sections/profile/token-icon.svg';
+import { RadioGroup } from '@/components/atoms';
 import { IProfileCard } from '@/types';
 
 /* eslint-disable react/react-in-jsx-scope */
 import { Card } from '../index';
 
 import './Content.scss';
-import { RadioGroup } from '@/components/atoms';
-import { RadioChangeEvent } from 'antd';
 
 const ITEMS: IProfileCard[] = Array(20)
   .fill('')
@@ -23,50 +23,76 @@ const ACTIVITY: IProfileCard[] = Array(5)
     tokenIcon: TokenIcon,
   }));
 
+const PAGES = [
+  { text: 'NFTs', value: 1 },
+  { text: 'Achivements', value: 2 },
+];
+
+const MODES = [
+  { text: 'Items', value: 1 },
+  { text: 'Activity', value: 2 },
+];
+
 const Content: FC = () => {
-  const [mode, setChangeMode] = useState(1);
+  const [NftData, setNftData] = useState<IProfileCard[]>(ITEMS);
+  const [achiveData] = useState<string>('Lorem ipsum'); // TODO setAchiveData
+  const [contentPage, setContentPage] = useState(1);
+  const [nftMode, setNftMode] = useState(1);
+
+  const handleChangeContentPage = (event: RadioChangeEvent) => {
+    const { value } = event.target;
+    setContentPage(value);
+  };
 
   const handleChangeMode = (event: RadioChangeEvent) => {
     const { value } = event.target;
-    setChangeMode(value);
+    setNftMode(value);
   };
+
+  const handleChangeNftData = useCallback(() => {
+    setNftData(nftMode === 1 ? ITEMS : ACTIVITY);
+  }, [nftMode]);
+
+  useEffect(() => {
+    handleChangeNftData();
+  }, [handleChangeNftData]);
 
   return (
     <div className="profile-content">
-      <RadioGroup
-        items={[
-          { text: 'Items', value: 1 },
-          { text: 'Activity', value: 2 },
-        ]}
-        value={mode}
-        onChange={handleChangeMode}
-        className="profile-content__radio"
-      />
+      <div className="profile-content__page">
+        <RadioGroup
+          value={contentPage}
+          onChange={handleChangeContentPage}
+          items={PAGES}
+          className="profile-content__page-radio"
+        />
+      </div>
       <div className="profile-content__body">
-        {mode === 1 &&
-          ITEMS.map(({ title, name, value, tokenIcon }) => {
-            return (
-              <Card
-                key={Math.random()}
-                title={title}
-                name={name}
-                value={value}
-                tokenIcon={tokenIcon}
-              />
-            );
-          })}
-        {mode === 2 &&
-          ACTIVITY.map(({ title, name, value, tokenIcon }) => {
-            return (
-              <Card
-                key={Math.random()}
-                title={title}
-                name={name}
-                value={value}
-                tokenIcon={tokenIcon}
-              />
-            );
-          })}
+        {contentPage === 1 ? (
+          <>
+            <RadioGroup
+              items={MODES}
+              value={nftMode}
+              onChange={handleChangeMode}
+              className="profile-content__body__mode"
+            />
+            <div className="profile-content__body__grid">
+              {NftData.map(({ title, name, value, tokenIcon }) => {
+                return (
+                  <Card
+                    key={Math.random()}
+                    title={title}
+                    name={name}
+                    value={value}
+                    tokenIcon={tokenIcon}
+                  />
+                );
+              })}
+            </div>
+          </>
+        ) : (
+          <div className="profile-content__body__grid">{achiveData}</div>
+        )}
       </div>
     </div>
   );
