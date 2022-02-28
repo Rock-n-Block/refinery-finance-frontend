2f9912757a8cdca6c9b5e72ed83d17ffd33796a3
Alexey Shevchenko <kamikozz@mail.ru>
2022-02-28 18:28:39 +0300

    chore: set actual dev build endpoint

--- src/core/axios.ts
+++ src/core/axios.ts
@@ -4,5 +4,5 @@ export const localApi = axios.create({
   baseURL:
     process.env.NODE_ENV === 'development'
       ? 'http://localhost:3000/'
-      : 'https://rifenery-dao.herokuapp.com/', // TODO: remove this when `git rebase` or `git merge`
+      : 'https://refineryfinance.herokuapp.com/',
 });
